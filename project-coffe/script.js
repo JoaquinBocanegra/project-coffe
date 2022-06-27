273e78ada5fb1c6b4093267e8ad949995d29fc3e
Joaquin Bocanegra <104942320+JoaquinBocanegra@users.noreply.github.com>
2022-06-27 18:26:48 -0300

    Primer commit

---
+++ project-coffe/script.js
@@ -0,0 +1,17 @@
+function fetchData() {
+    fetch("coffe.json")
+        .then(res => res.json())
+        .then((data) => {
+            let output = "<table>" + "<tbody>"
+            data.forEach(element => {
+                output += `<tr><td>${element.tipo}</td><td>${element.precio}</td></tr>`
+
+            });
+
+            output += "</tbody>";
+            output += "</table>";
+            document.getElementById("wrapper").innerHTML = output;
+        })
+        .catch(console.log("Error"));
+}
+fetchData();
